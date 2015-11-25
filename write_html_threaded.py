e36cd1885c23e331441c0ab525dfd33ba9614269
Ryan Cox <rbc@Ryans-MacBook-Pro-3.local>
2015-11-25 14:02:36 -0500

    tweaks script

--- write_html_threaded.py
+++ write_html_threaded.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
-# prints files of the format [randint].[timestamp].html
-# in folder [ebay_search_word]_files_[timestamp] in working dir
+# hits ebay with given search_word and writes html DOM locally
+# prints files of the format [randint].[timestamp].html in folder [ebay_search_word]_files_[timestamp] in working dir
 import urllib2, time, Queue, threading, os
 from random import randint
 
 num_threads = 50
 hits_per_thread = 100
-search_word = 'aeroplane'
+#search_word = 'aeroplane'
+#search_word = 'gun'
+search_word = 'stapler'
 url = 'http://www.ebay.com/sch/i.html?_odkw='+search_word+'&_osacat=0&_from=R40&_trksid=p2045573.m570.l1313.TR12.TRC2.A0.H0.TRS0&_nkw='+search_word+'&_sacat=0' 
 
 def request(url):
@@ -23,11 +25,11 @@ def write_lines(out_files_dir,hits_per_thread):
   '''write DOM to local file.'''
   counter = 0
   while counter < hits_per_thread:
-    #files named with random int preceding so they won't clobber one another
-    fn = str(randint(0,1000000))+'.'+str(time.time())+'.html'
-    f = open(fn, 'w')
-    print "opening file " + fn
     try:
+      #files named with random int preceding so they won't clobber one another
+      fn = str(randint(0,1000000))+'.'+str(time.time())+'.html'
+      f = open(fn, 'w')
+      print "opening file " + fn
       the_page = request(url)
       f.write(the_page)
       counter = counter + 1
